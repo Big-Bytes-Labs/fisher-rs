8f214220f162a039d62e474ef6fd887b81f38d31
vilayat-ali <vilayatcodemysite@gmail.com>
2023-08-22 18:04:24 +0530

    Init

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,9 @@
-use std::{collections::HashMap, default::Default, fmt};
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-mod math;
-
-#[derive(Debug)]
-pub struct DF {
-    pub frame: HashMap<String, Vec<String>>,
-    pub shape: (usize, usize),
-}
-
-impl Default for DF {
-    fn default() -> Self {
-        Self {
-            frame: HashMap::new(),
-            shape: (0, 0),
-        }
-    }
-}
-
-impl fmt::Display for DF {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Hello")
-    }
-}
-
-impl DF {
-    pub fn read_csv(path_to_csv: &str) -> Result<String, String> {
-        Ok(String::new())
+    #[test]
+    fn main_test() {
+        assert_eq!(2 + 2, 5);
     }
 }
