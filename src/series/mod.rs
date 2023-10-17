e0b24202ef5c968d36c015d1e805b84438d2fbb4
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-17 13:18:55 +0530

    added len for series

--- src/series/mod.rs
+++ src/series/mod.rs
@@ -1,21 +1,29 @@
+pub mod float_series;
+pub mod int_series;
+pub mod str_series;
+
+#[derive(Debug)]
 pub enum SeriesType {
-    Str(Box<Vec<String>>),
-    Flt(Box<Vec<f64>>),
-    Int(Box<Vec<usize>>),
+    Str(Vec<String>),
+    Flt(Vec<f64>),
+    Int(Vec<usize>),
 }
 
+#[derive(Debug)]
 pub struct Series (SeriesType);
 
 impl Series {
     pub fn new() -> Self {
         Self (
-            SeriesType::Int(Box::new(Vec::new()))
+            SeriesType::Int(Vec::new())
         )
     }
+
+    pub fn len(&self) -> usize {
+        match &self.0 {
+            SeriesType::Str(ref v) => v.len(),
+            SeriesType::Flt(ref v) => v.len(),
+            SeriesType::Int(ref v) => v.len(),
+        }
+    }
 }
-
-pub trait StrSeries {}
-
-pub trait FltSeries {}
-
-pub trait IntSeries {}
