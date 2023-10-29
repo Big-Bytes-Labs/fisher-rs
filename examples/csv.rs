4e8c693b2fc9abd4f4b5ec70ed520acab0ef909d
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-29 16:31:57 +0530

    fixed clippy errors

--- examples/csv.rs
+++ examples/csv.rs
@@ -3,7 +3,7 @@ use fisher::series::Series;
 use fisher::series::str_series::StrSeries;
 
 fn main() {
-    let mut s = Series::Str(vec!["ali".to_owned(), "mohd".to_owned()]);
+    let s: Series = Series::Str(vec!["ali".to_owned(), "mohd".to_owned()]);
     let s = s.cat(Some(" bin "));
 
     println!("{:#?}", s);
