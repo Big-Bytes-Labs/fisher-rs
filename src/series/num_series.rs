2dab015ee947f810b3a633deb4a1160c9ff8b2ea
vilayat-ali <vilayatcodemysite@gmail.com>
2023-10-29 17:06:38 +0530

    Feature: Added trait methods for NumSeries

--- src/series/num_series.rs
+++ src/series/num_series.rs
@@ -1,1 +1,21 @@
-pub trait NumSeries {}
+pub trait NumSeries {
+    fn sum(&self) -> f64;
+    fn product(&self) -> f64;
+    fn mean(&self) -> f64;
+    fn median(&self) -> f64;
+    fn mode(&self) -> f64;
+    fn std_dev(&self) -> f64;
+    fn min(&self) -> f64;
+
+    fn add(&self) -> f64;
+    fn sub(&self) -> f64;
+    fn mul(&self) -> f64;
+    fn div(&self) -> f64;
+
+    fn corr(&self) -> f64; // calculates correlation matrix
+    fn cov(&self) -> f64; // calculates covariance matrix
+    fn max(&self) -> f64;
+    
+    fn drop_na(&self); // clears null/empty values
+    fn fill_na(&self); // fill empty values
+}
