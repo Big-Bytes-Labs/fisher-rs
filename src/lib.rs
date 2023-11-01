93fa32a8cbebf9d3f5d959e4d2a8cdaa9b9607e0
vilayat-ali <vilayatcodemysite@gmail.com>
2023-11-01 21:08:16 +0530

    removed prelude

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,35 @@
 /// For more examples and detailed usage, check out the documentation.
 /// 
 
-pub mod prelude;
 pub mod series;
 pub mod errors;
 pub mod results;
+
+pub use std::collections::HashMap;
+
+pub struct Dataframe {
+    pub frame: HashMap<String, String>,
+    pub size: (usize, usize),
+}
+
+pub trait DataFrame {
+    fn generate_frame() -> Self;
+
+    fn print_first(&self);
+
+    fn print_last(&self);
+}
+
+impl DataFrame for Dataframe {
+    fn generate_frame() -> Self {
+        todo!()
+    }
+
+    fn print_first(&self) {
+        
+    }
+
+    fn print_last(&self) {
+        
+    }
+}
