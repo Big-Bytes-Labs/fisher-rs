9a5572895515ad56f4325965fdf0c3eee6a3523e
vilayat-ali <vilayatcodemysite@gmail.com>
2023-11-01 20:27:44 +0530

    completed all method implementations for NumSeries trait impl over Series

--- src/series/num_series.rs
+++ src/series/num_series.rs
@@ -1,25 +1,25 @@
 use super::Series;
 
+use std::collections::HashMap;
+
 pub trait NumSeries {
     fn sum(&self) -> Option<f64>;
     fn product(&self) -> Option<f64>;
+
     fn mean(&self) -> Option<f64>;
     fn median(&self) -> Option<f64>;
     fn mode(&self) -> Option<f64>;
     fn std_dev(&self) -> Option<f64>;
+
+    fn max(&self) -> Option<f64>;
     fn min(&self) -> Option<f64>;
 
-    fn add(&self) -> Option<f64>;
-    fn sub(&self) -> Option<f64>;
-    fn mul(&self) -> Option<f64>;
-    fn div(&self) -> Option<f64>;
+    fn add(&mut self, num: f64);
+    fn sub(&mut self, num: f64);
+    fn mul(&mut self, num: f64);
+    fn div(&mut self, num: f64);
 
-    fn corr(&self) -> Option<f64>; // calculates correlation matrix
-    fn cov(&self) -> Option<f64>; // calculates covariance matrix
-    fn max(&self) -> Option<f64>;
-    
-    fn drop_na(&self); // clears null/empty values
-    fn fill_na(&self); // fill empty values
+    fn replace_with(&mut self, val: Option<f64>); // fill empty values
 }
 
 impl NumSeries for Series {
@@ -48,15 +48,50 @@ impl NumSeries for Series {
     }
 
     fn median(&self) -> Option<f64> {
-        todo!()
+        if let Series::Num(ref vec) = &self {
+            if vec.len() == 0 {
+                return None;
+            }
+            let mut sorted_vec: Vec<f64> = vec.clone();
+            sorted_vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            match sorted_vec.len() % 2 {
+                0 => Some(sorted_vec[sorted_vec.len() / 2]),
+                _ => Some(sorted_vec[(sorted_vec.len() + 1) / 2])
+            }
+        } else {
+            None
+        }
     }
 
     fn mode(&self) -> Option<f64> {
-        todo!()
+        if let Series::Num(ref vec) = &self {
+            let mut frequency_map: HashMap<String, usize> = HashMap::new();
+            for el in vec.iter() {
+                let count = frequency_map.entry(el.to_string()).or_insert(0);
+                *count += 1;
+            }
+            match frequency_map.into_iter().max_by_key(|el| el.1) {
+                Some((el, _)) => Some(el.parse::<f64>().unwrap()),
+                None => None
+            }
+        } else {
+            None
+        }
     }
 
     fn std_dev(&self) -> Option<f64> {
-        todo!()
+        if let Series::Num(ref vec) = self {
+            match self.mean() {
+                Some(mean) => {
+                    let squared_diff_sum: f64 = vec.clone().into_iter().map(|el| ((el - mean) as f64).powi(2)).sum();
+                    let variance = squared_diff_sum / (vec.len() as f64);
+                    Some(variance.sqrt())
+                },
+                None => None
+            }
+        } else {
+            None
+        }
     }
 
     fn min(&self) -> Option<f64> {
@@ -87,35 +122,37 @@ impl NumSeries for Series {
         }
     }
 
-    fn add(&self) -> Option<f64> {
-        todo!()
+    fn add(&mut self, num: f64) {
+        if let Series::Num(ref mut vec) = self {
+            vec.iter_mut().for_each(|el| *el += num)
+        } 
     }
 
-    fn sub(&self) -> Option<f64> {
-        todo!()
+    fn sub(&mut self, num: f64) {
+        if let Series::Num(ref mut vec) = self {
+            vec.iter_mut().for_each(|el| *el -= num)
+        } 
     }
 
-    fn mul(&self) -> Option<f64> {
-        todo!()
+    fn mul(&mut self, num: f64) {
+        if let Series::Num(ref mut vec) = self {
+            vec.iter_mut().for_each(|el| *el *= num)
+        } 
     }
 
-    fn div(&self) -> Option<f64> {
-        todo!()
+    fn div(&mut self, num: f64) {
+        if let Series::Num(ref mut vec) = self {
+            vec.iter_mut().for_each(|el| *el = *el / num)
+        } 
     }
 
-    fn corr(&self) -> Option<f64> {
-        todo!()
+    fn replace_with(&mut self, val: Option<f64>) {
+        if let Series::Num(ref mut vec) = self {
+            vec.iter_mut()
+                .for_each(|el| {
+                    *el = val.unwrap_or_default()
+                })
+        }
     }
 
-    fn cov(&self) -> Option<f64> {
-        todo!()
-    }
-
-    fn drop_na(&self) {
-        todo!()
-    }
-
-    fn fill_na(&self) {
-        todo!()
-    }
 }
